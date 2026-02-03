67cbe6b9d7706ea025100cb1cfb62dda22a95a3f
Lena Thomas <24211a05y1@bvrit.ac.in>
2026-02-03 23:17:12 +0530

    game_score.py

--- game_score.py
+++
@@ -1,8 +0,0 @@
-player=input("Enter player's name:")
-games_played=int(input("Enter the number of games played:"))
-score=int(input("Enter total score:"))
-avg_score=score/games_played
-print("Player: ",player)
-print(f"Games Played: {games_played}")
-print(f"Total Score: {score}")
-print(f"Average Score: {avg_score}")
